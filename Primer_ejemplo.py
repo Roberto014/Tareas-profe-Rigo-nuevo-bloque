257a4909303f08cad1b859af2d5d8fd09e2b52cb
Roberto014 <A01424631@tec.mx>
2024-10-30 17:04:10 -0600

    agregar ciclo for

--- Primer_ejemplo.py
+++ Primer_ejemplo.py
@@ -1,1 +1,7 @@
 print("Hola a todos, este es un repositorio local y se desplegara en GitHub")
+
+print ("Este archivo se estara modificando continuamente")
+
+for i in range(10):
+    print(i)
+    
